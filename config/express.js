519ee7fdbc3bedb73abafafffe748911df99bb17
Joseph <joseluis.bsa@gmail.com>
2018-06-17 09:44:43 -0300

    added helmet package

--- config/express.js
+++ config/express.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
+
 
 module.exports = () => {
     const app = express()
 
+    app.use(helmet())
     app.use(bodyParser.urlencoded({ extended: true}))
     app.use(bodyParser.json())
 
