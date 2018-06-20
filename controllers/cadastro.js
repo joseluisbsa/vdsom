cda0f43405196224daffa44a5ef24122fa266a80
Joseph <joseluis.bsa@gmail.com>
2018-06-20 01:03:54 -0300

    added ponto e virgula

--- controllers/cadastro.js
+++ controllers/cadastro.js
@@ -1,24 +1,24 @@
 module.exports = app => {
     app.get('/cadastro', (req, res) => {
-        console.log('page cadastro opened')
-        res.send('OK')
+        console.log('page cadastro opened');
+        res.send('OK');
     })
 
     app.post('/cadastro/adicionar', (req, res) => {
-        
+
         req.assert('nome', 'Nome obrigatorio').notEmpty();
         req.assert('idade', 'Idade obrigatoria').notEmpty();
 
         const erros = req.validationErrors();
 
-        if(erros) {
+        if (erros) {
             console.log('Erro em algum campo');
-            res.status(400).send(erros)
+            res.status(400).send(erros);
             return;
         }
 
         let aluno = req.body;
-        
+
         console.log('req recebecido em /cadastro/adicionar');
 
         let alunoDao = new app.DAO.AlunoDao(req.connection);
@@ -26,10 +26,12 @@ module.exports = app => {
         alunoDao.criarAluno(aluno, (erro, resultado) => {
             if (erro) {
                 console.log('Erro DB: ' + erro);
-                res.status(400).send(erro);
+                res.status(500).send(erro);
             } else {
                 console.log('aluno criado');
-                res.json(aluno);
+
+                res.location('/cadastro/adicionar/' + resultado.insertId);
+                res.status(201).json(aluno);
             }
         });
     });
