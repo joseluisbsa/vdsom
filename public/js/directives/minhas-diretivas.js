ba61e0a0a787befc3d17590d7e342b34cbb7d2f8
Joseph <joseluis.bsa@gmail.com>
2018-06-25 23:50:38 -0300

    creating front-end

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,15 +1,18 @@
 angular.module('minhasDiretivas', [])
     .directive('meuPainel', function () {
+        // directive definition object
         let ddo = {};
-
+        // restricao de uso: 'A' attr 'E' elem
         ddo.restric = "AE";
 
         ddo.scope = {
-            nome: '@'
+            // @nome se refere ao attr do html principal
+            // nome: se refere ao {{}} do html da diretiva
+            nome: '@nome'
         };
-
+        // para manter tags existentes no html principal
         ddo.transclude = true;
-
+        // uso de base pasta public
         ddo.templateUrl = 'js/directives/meu-painel.html';
 
         return ddo;
