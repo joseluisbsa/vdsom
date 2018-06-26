fae6d4602ee111fedde562c76bbff18495c31d6d
Joseph <joseluis.bsa@gmail.com>
2018-06-26 00:21:51 -0300

    creating front-end

--- public/js/controllers/nomes-controller.js
+++ public/js/controllers/nomes-controller.js
@@ -1,6 +1,9 @@
 angular.module('vdsom').controller('NomesController', 
                                     function($scope, $http){
 
+    $scope.nomes = [];
+    $scope.filtro = '';
+
     $http.get('/test')
     .success( nomes => {
         $scope.nomes = nomes;
