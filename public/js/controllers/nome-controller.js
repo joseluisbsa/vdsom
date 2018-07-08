f1996bfde6cbeaef8a7dcc2478d06c48342572e9
Joseph <joseluis.bsa@gmail.com>
2018-07-08 00:54:52 -0300

    added controller to valid fields

--- public/js/controllers/nome-controller.js
+++ public/js/controllers/nome-controller.js
@@ -1,9 +1,20 @@
 angular.module('vdsom').controller('NomeController', 
-function($scope) {
+function($scope, $http) {
     
     $scope.nome = {};
+    $scope.mensagem = '';
 
     $scope.salvarNome = function() {
-        console.log($scope.nome)
+        if($scope.formulario.$valid) {
+            $http.post('/alunos', $scope.nome)
+            .success(function() {
+                $scope.nome = {};
+                $scope.mensagem = 'Foto incluída com sucesso';
+            })
+            .error(function(erro){
+                $scope.mensagem = 'Não foi possível incluir a foto';
+                console.log(erro);
+            });
+        }
     };
 })
