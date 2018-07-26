a9032bb66dab5c6b9b98be5b784e73f3890b139c
Joseph <joseluis.bsa@gmail.com>
2018-07-25 23:02:50 -0300

    added put method

--- public/js/controllers/nome-controller.js
+++ public/js/controllers/nome-controller.js
@@ -1,11 +1,32 @@
 angular.module('vdsom').controller('NomeController', 
-function($scope, $http) {
+function($scope, $http, $routeParams) {
     
     $scope.nome = {};
     $scope.mensagem = '';
 
+    if($routeParams.nomeId){
+        $http.get('url/uri' + $routeParams.nomeId)
+        .success(function(nome){
+            // $scope.nome = nome;
+        })
+        .error(function(erro){
+            console.log(erro);
+            //$scope.mensagem = "Erro ao buscar foto";
+        })
+    }
+
     $scope.salvarNome = function() {
         if($scope.formulario.$valid) {
+            if($scope.nome.id){
+                $http.put('url/uri' + $scope.nome.id, $scope.nome)
+                .success(function(){
+                    //mensagem
+                })
+                .error(function(erro){
+                    console.log(erro);
+                    //mensagem
+                })
+            }
             $http.post('/alunos', $scope.nome)
             .success(function() {
                 $scope.nome = {};
