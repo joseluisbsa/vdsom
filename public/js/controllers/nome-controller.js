36a549c09d5338fac851ff1f2ed92986aff8ce29
Joseph <joseluis.bsa@gmail.com>
2018-08-01 22:26:51 -0300

    throw the resposability to meus-serviços

--- public/js/controllers/nome-controller.js
+++ public/js/controllers/nome-controller.js
@@ -1,41 +1,29 @@
-angular.module('vdsom').controller('NomeController', 
-function($scope, $http, $routeParams) {
-    
-    $scope.nome = {};
-    $scope.mensagem = '';
+angular.module('vdsom').controller('NomeController',
+    function ($scope, cadastroDeNomes, recursoNome, $routeParams) {
 
-    if($routeParams.nomeId){
-        $http.get('url/uri' + $routeParams.nomeId)
-        .success(function(nome){
-            // $scope.nome = nome;
-        })
-        .error(function(erro){
-            console.log(erro);
-            //$scope.mensagem = "Erro ao buscar foto";
-        })
-    }
+        $scope.nome = {};
+        $scope.mensagem = '';
 
-    $scope.salvarNome = function() {
-        if($scope.formulario.$valid) {
-            if($scope.nome.id){
-                $http.put('url/uri' + $scope.nome.id, $scope.nome)
-                .success(function(){
-                    //mensagem
-                })
-                .error(function(erro){
-                    console.log(erro);
-                    //mensagem
-                })
-            }
-            $http.post('/alunos', $scope.nome)
-            .success(function() {
-                $scope.nome = {};
-                $scope.mensagem = 'Foto incluída com sucesso';
-            })
-            .error(function(erro){
-                $scope.mensagem = 'Não foi possível incluir a foto';
+        if ($routeParams.nomeId) {
+
+            recursoNome.get({ nomeId: $routeParams.nomeId }, function (nome) {
+                // $scope.nome = nome;
+            }, function (erro) {
                 console.log(erro);
-            });
+                //$scope.mensagem = "Erro ao buscar foto";
+            })
         }
-    };
-})
+
+        $scope.salvarNome = function () {
+            if ($scope.formulario.$valid) {
+                cadastroDeNomes.cadastrar($scope.nome)
+                    .then(function (dados) {
+                        $scope.mensagem = dados.mensagem;
+                        if (dados.inclusa) $scope.nome = {};
+                    })
+                    .catch(function (dados) {
+                        $scope.mensagem = dados.mensagem;
+                    })
+            }
+        };
+    })
